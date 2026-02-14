b798f23a45cb1f0a65d784266c74b1d8137366d9
Ajnthony <66324186+Ajnthony@users.noreply.github.com>
2026-02-13 16:25:47 -0800

    add tests for both apps

--- apikeymgr/apps/key/tests.py
+++ apikeymgr/apps/key/tests.py
@@ -1,43 +1,49 @@
 from django.test import TestCase, Client
-from django.utils import timezone
+from django.contrib.auth import get_user_model
 from .models import APIKey
-from user.models import User
 
 
 class APIKeyTest(TestCase):
     def setUp(self):
-        user_one = User.objects.create(
-            id=1,
-            full_name="",
-            username="user19345",
+        User = get_user_model()
+        User.objects.create_user(
             email="user1@test.com",
-            created_at=timezone.now,
-            updated_at=None,
+            password="1234",
             plan="free",
             is_suspended=False,
             is_staff=False,
             is_active=True,
         )
-        user_two = User.objects.create(
-            id=2,
-            full_name="",
-            username="user23257",
+        User.objects.create_user(
             email="user2@test.com",
-            created_at=timezone.now,
-            updated_at=None,
+            password="1234",
             plan="pro",
             is_suspended=False,
             is_staff=False,
             is_active=True,
         )
 
+    def test_number_of_api_keys_match(self):
+        user_one = get_user_model().objects.get(email="user1@test.com")
+        user_two = get_user_model().objects.get(email="user2@test.com")
+
         APIKey.objects.create(id=1, user=user_one, api_key_hash="abc123")
         APIKey.objects.create(id=2, user=user_two, api_key_hash="def456")
         APIKey.objects.create(id=3, user=user_one, api_key_hash="ghi789")
 
-    def test_api_key_has_fields(self):
-        user_one_keys = APIKey.objects.filter(user=1)
-        user_two_keys = APIKey.objects.filter(user=2)
+        user_one_keys = APIKey.objects.filter(user=user_one)
+        user_two_keys = APIKey.objects.filter(user=user_two)
 
         self.assertEqual(user_one_keys.count(), 2)
         self.assertEqual(user_two_keys.count(), 1)
+
+    def test_api_key_has_fields(self):
+        user_two = get_user_model().objects.get(email="user2@test.com")
+        APIKey.objects.create(id=2, user=user_two, api_key_hash="def456")
+
+        user_two_key = APIKey.objects.get(user=user_two)
+
+        self.assertEqual(user_two_key.is_active, True)
+        self.assertEqual(user_two_key.daily_use_count, 0)
+        self.assertEqual(user_two_key.total_use_count, 0)
+        self.assertEqual(user_two_key.revoked_at, None)
--- apikeymgr/apps/user/tests.py
+++ apikeymgr/apps/user/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class APIKeyTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        User.objects.create_user(
+            # id=1,
+            # full_name="",
+            # username="user19345",
+            email="user1@test.com",
+            password="1234",
+            # created_at=timezone.now,
+            # updated_at=None,
+            plan="free",
+            is_suspended=False,
+            is_staff=False,
+            is_active=True,
+        )
+        User.objects.create_user(
+            # id=2,
+            # full_name="",
+            # username="user23257",
+            email="user2@test.com",
+            password="1234",
+            # created_at=timezone.now,
+            # updated_at=None,
+            plan="pro",
+            is_suspended=False,
+            is_staff=False,
+            is_active=True,
+        )
+
+    def test_user_created(self):
+        user_one = get_user_model().objects.get(email="user1@test.com")
+        user_two = get_user_model().objects.get(email="user2@test.com")
+
+        self.assertEqual(user_one.plan, "free")
+        self.assertEqual(user_two.plan, "pro")
