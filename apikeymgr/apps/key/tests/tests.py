7abf3345c983afe14eee68c791e41a78d4d2e6c9
Ajnthony <66324186+Ajnthony@users.noreply.github.com>
2026-02-14 16:07:21 -0800

    move test api key creation to setup

--- apikeymgr/apps/key/tests/tests.py
+++ apikeymgr/apps/key/tests/tests.py
@@ -4,9 +4,12 @@ from apikeymgr.apps.key.models import APIKey
 
 
 class APIKeyTest(TestCase):
+    c = Client()
+
     def setUp(self):
         User = get_user_model()
-        User.objects.create_user(
+
+        user_one = User.objects.create_user(
             email="user1@test.com",
             password="1234",
             plan="free",
@@ -14,7 +17,7 @@ class APIKeyTest(TestCase):
             is_staff=False,
             is_active=True,
         )
-        User.objects.create_user(
+        user_two = User.objects.create_user(
             email="user2@test.com",
             password="1234",
             plan="pro",
@@ -23,13 +26,13 @@ class APIKeyTest(TestCase):
             is_active=True,
         )
 
-    def test_number_of_api_keys_match(self):
-        user_one = get_user_model().objects.get(email="user1@test.com")
-        user_two = get_user_model().objects.get(email="user2@test.com")
-
         APIKey.objects.create(id=1, user=user_one, api_key_hash="abc123")
         APIKey.objects.create(id=2, user=user_two, api_key_hash="def456")
         APIKey.objects.create(id=3, user=user_one, api_key_hash="ghi789")
+
+    def test_number_of_api_keys_match(self):
+        user_one = get_user_model().objects.get(email="user1@test.com")
+        user_two = get_user_model().objects.get(email="user2@test.com")
 
         user_one_keys = APIKey.objects.filter(user=user_one)
         user_two_keys = APIKey.objects.filter(user=user_two)
@@ -39,7 +42,6 @@ class APIKeyTest(TestCase):
 
     def test_api_key_has_fields(self):
         user_two = get_user_model().objects.get(email="user2@test.com")
-        APIKey.objects.create(id=2, user=user_two, api_key_hash="def456")
 
         user_two_key = APIKey.objects.get(user=user_two)
 
@@ -47,3 +49,6 @@ class APIKeyTest(TestCase):
         self.assertEqual(user_two_key.daily_use_count, 0)
         self.assertEqual(user_two_key.total_use_count, 0)
         self.assertEqual(user_two_key.revoked_at, None)
+
+    def test_soft_delete(self):
+        pass
